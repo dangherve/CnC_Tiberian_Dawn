79b2336bc780307e887c4037f1d4fccbb3d1af1f
dangherve <dangherve@gmail.com>
2026-03-12 13:40:45 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/electronicarts/CnC_Tiberian_Dawn.git
+git fetch upstream
+git rebase upstream/main
+git remote set-url origin git@github.com:dangherve/CnC_Tiberian_Dawn.git
+git push --force --set-upstream origin main
